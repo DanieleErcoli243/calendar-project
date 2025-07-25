1b4058062200830d2cfa497aee0e17705804c1c8
danieleercoli243 <danielercoli@gmail.com>
2025-07-25 18:14:05 +0200

    estratto le variabili sui nuovi appuntamenti

--- calendar.php
+++ calendar.php
@@ -8,3 +8,14 @@ $success_msg = '';
 $error_msg = '';
 // un array per contenere gli eventi raccolti
 $events_from_db = [];
+
+// gestire l'aggiunta di un appuntamento
+
+if ($_SERVER("REQUEST_METHOD") === "POST" && ($_POST['action'] ?? '') === "add") {
+    $course = trim($_POST["course_name"] ?? "");
+    $instructor = trim($_POST["instructor_name"] ?? "");
+    $start = $_POST["start_date"] ?? "";
+    $end = $_POST["end_date"] ?? "";
+
+    
+}
