74c4ab69d47c91cc9703304ee77ce6167f71f3a1
danieleercoli243 <danielercoli@gmail.com>
2025-07-25 18:04:08 +0200

    creted three variables

--- calendar.php
+++ calendar.php
@@ -1,3 +1,10 @@
 <?php
 // dobbiamo includere il file connection per connettersi con la bancadati
 include "connection.php"
+
+// creo tre variabili
+
+$success_msg = '';
+$error_msg = '';
+// un array per contenere gli eventi raccolti
+$events_from_db = [];
