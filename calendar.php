5b88efccf037fd27c085ac10dd68181f401a4617
danieleercoli243 <danielercoli@gmail.com>
2025-07-25 18:28:13 +0200

    wrote the code to handle the addition of an appointment

--- calendar.php
+++ calendar.php
@@ -12,10 +12,28 @@ $events_from_db = [];
 // gestire l'aggiunta di un appuntamento
 
 if ($_SERVER("REQUEST_METHOD") === "POST" && ($_POST['action'] ?? '') === "add") {
+    // se ci sono, salvo i dati nelle variabili 
     $course = trim($_POST["course_name"] ?? "");
     $instructor = trim($_POST["instructor_name"] ?? "");
     $start = $_POST["start_date"] ?? "";
     $end = $_POST["end_date"] ?? "";
-
     
+    // scrivo una stringa sql
+
+    if ($course && $instructor && $instructor && $end) {
+        $stmt = $conn->prepare(
+            "INSERT INTO appointments (course_name, instructor_name, start_date, end_date) VALUES (?, ?, ?, ?)"
+        ); 
+    }
+
+    $stmt->bind_param("ssss", $course, $instructor, $start, $end);
+
+    $stmt->execute();
+    $stmt->close();
+
+    header("Location: " . $_SERVER["PHP_SELF"] . "?success=1");
+    exit;
+} else {
+    header("Location: " . $_SERVER["PHP_SELF"] . "?error=1");
+    exit;
 }
