87117a2bf64e3db3a668ac19b5a8871419dd3d47
danieleercoli243 <danielercoli@gmail.com>
2025-07-26 11:46:37 +0200

    still working on the function

--- js/script.js
+++ js/script.js
@@ -9,6 +9,27 @@ const renderCalendar = (date = new Date()) => {
     // istanzio una variabile per concatenare la data
     const totalDays = new Date(0, month+ 1, year).getDate();
     // istanzio una variabile per trovare il primo giorno del mese
+    const firstDayOfMonth = new Date(1,month, year).getDay();
+    // mostro il mese e l'anno
+    monthYearElmnt.textContent = date.toLocaleString('it', {
+        month: 'long',
+        year: 'numeric'
+    }
+
+    );
+
+    // creo un array coi giorni della settimana
+    const weekdays = ['lun', 'mar', 'mer', 'gio', 'ven', 'sab', 'dom'];
+    // faccio un ciclo sull'array dei giorni per creare dinamicamente un div per ogni giorno
+    weekdays.forEach(day => {
+        const dayElmnt = document.createElement("div");
+        // do una classe all'elemento 
+        dayElmnt.className = "day-name";
+        // lo riempio
+        dayElmnt.textContent = day;
+        // lo appendo
+        calendarElmnt.appendChild(dayElmnt);
+    })
 }
 
 // seleziono gli elementi dal DOM
